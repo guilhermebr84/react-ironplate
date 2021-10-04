6e44c6f90c72de909ecd31e86305f4d6687da002
Guilherme Rosa <guilhermebr84@gmail.com>
2021-10-04 10:23:40 -0300

    front 21-10-04

--- src/routeComponents/Home.js
+++ src/routeComponents/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
+import logo1 from "../assets/styles/img/logo1.png";
 import { Link } from "react-router-dom";
+import "../assets/styles/index.css"
 
 function Home() {
   return (
-    <div className="text-center">
-      <img
-        src="https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/84/original/logo-ironhack-blue.png"
-        alt="ironhack logo"
-      />
-      <h1>React IronPlate</h1>
-      <p>This is the homepage</p>
-      <div className="d-flex flex-column align-items-center">
-        <Link className="btn btn-lg btn-primary" to="/auth/signup">
-          Signup here!
-        </Link>
+    <div className="bg-home">
+      <div className="text-center">
+        <img src={logo1} alt="company logo" />
+        <h1>Rivals F.C.</h1>
+        <p>Find your Rival and Try to Win the Game!</p>
+        <div className="d-flex flex-column align-items-center">
+          <Link className="btn btn-lg btn-primary" to="/auth/signup">
+            Signup here!
+          </Link>
+        </div>
       </div>
     </div>
   );
